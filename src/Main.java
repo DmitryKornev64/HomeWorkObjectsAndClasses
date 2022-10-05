1ad3c802aeb0e6e8cfd24c1d0506fdf04d62a30c
dmitry <dmitrykornev.64@gmail.com>
2022-10-06 02:31:29 +0400

    ДЗ Объекты и классы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author stephenKing = new Author("Stephen", "King");
+        Book theStand = new Book("The Stand", stephenKing, 1975);
+
+
+        Author levTolstoy = new Author("Lev", "Tolstoy");
+        Book warAndPeace = new Book("War and Peace", levTolstoy, 1865);
+
+
+        System.out.println("Название книги: " + theStand.getTitle()
+        + " Автор: " + theStand.getAuthor().getName() + " " + theStand.getAuthor().getSurname()
+        + " Дата: " +theStand.getPublicationYear());
+
+
+        System.out.println("Название книги: " + warAndPeace.getTitle()
+                + " Автор: " + warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getSurname()
+                + " Дата: " + warAndPeace.getPublicationYear());
+
+
+        warAndPeace.setPublicationYear(1999);
+        System.out.println("Название книги: " + warAndPeace.getTitle()
+                + " Автор: " + warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getSurname()
+                + " Дата: " + warAndPeace.getPublicationYear());
     }
 }
